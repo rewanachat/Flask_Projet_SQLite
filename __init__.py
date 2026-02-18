d115b5cee08842ca05f36921d1d22ec6d2ab4a3d
rewanachat <rewanrecovery@outlook.fr>
2026-02-18 14:46:52 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ def est_authentifie():
 
 @app.route('/')
 def hello_world():
-    return render_template('hello.html')
+    return render_template('index.html')
 
 @app.route('/lecture')
 def lecture():
