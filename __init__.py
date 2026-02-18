0fdb1067e2398cca81477e725928a255f5e47014
rewanachat <rewanrecovery@outlook.fr>
2026-02-18 17:06:07 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -89,3 +89,21 @@ def auth_user():
 
 if __name__ == "__main__":
     app.run(debug=True)
+import os
+import sqlite3
+from flask import g
+
+DATABASE = os.path.join(os.path.dirname(__file__), 'data', 'tasks.sqlite')
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = sqlite3.Row
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
